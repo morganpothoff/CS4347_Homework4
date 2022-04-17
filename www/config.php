0d318ac99215de6e53ad2b226b824223b4e4ca5f
Thomas Bennett <95662897+trb090020@users.noreply.github.com>
2022-04-17 16:26:08 -0500

    Delete config.php

--- www/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$host="ls-99428edba7c928b0c62ad0e6cd11c0ece8660db3.ciw1f5witzxf.us-east-2.rds.amazonaws.com";
-	$db="BaseballLeague";
-	$user="dbmasteruser";
-	$password="masterblaster";
-	
-	$PGDB= pg_connect(" host=$host port=5432 dbname=$db user=$user password=$password ") or die();
-?>
